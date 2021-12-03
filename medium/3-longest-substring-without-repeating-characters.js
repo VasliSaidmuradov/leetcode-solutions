4b95edc80b0a9a73b0242c5efe5ae14c21cf4cb8
Vasli Saidmuradov <vasli.said@gmail.com>
2021-12-03 23:07:06 +0600

    feat: solution to #3

---
+++ medium/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,90 @@
+// Given a string s, find the length of the longest substring without repeating characters.
+
+ 
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+// Example 2:
+
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+// Example 3:
+
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+// Example 4:
+
+// Input: s = ""
+// Output: 0
+ 
+
+// Constraints:
+
+// 0 <= s.length <= 5 * 104
+// s consists of English letters, digits, symbols and spaces.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+// Solutions
+
+// #1
+const lengthOfLongestSubstring = function(s) {
+  let max = ''
+  let temp = ''
+  const map = new Map()
+
+  let i = 0
+  let j = 0
+
+  while (j < s.length) {
+    if (!map.has(s[j])) {
+      map.set(s[j], true)
+      temp += s[j]
+      j++
+    } else {
+      if (temp.length > max.length) {
+          max = temp
+      }
+      temp = ''
+      map.clear()
+      i++
+      j = i
+    }
+  }
+  
+  if (temp.length > max.length) {
+    max = temp
+  }
+
+  return max.length
+}
+
+
+// #2
+const lengthOfLongestSubstring = function(s) {
+  let longest = 0
+  let seen = {}
+  let start = 0
+
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i]
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
+    // index - beginning of substring + 1 (to include current in count)
+    longest = Math.max(longest, i - start + 1)
+    // store the index of the next char so as to not double count
+    seen[char] = i + 1
+  }
+
+  return longest
+}
