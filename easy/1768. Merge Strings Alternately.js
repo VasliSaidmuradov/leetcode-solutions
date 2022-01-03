408f89f53e1839ce7f85b8316043c980fe7a8141
Vasli Saidmuradov <vasli.said@gmail.com>
2022-01-03 17:16:02 +0600

    feat: solution to #1768

---
+++ easy/1768. Merge Strings Alternately.js
@@ -0,0 +1,81 @@
+// You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+// Return the merged string.
+
+// Example 1:
+
+// Input: word1 = "abc", word2 = "pqr"
+// Output: "apbqcr"
+// Explanation: The merged string will be merged as so:
+// word1:  a   b   c
+// word2:    p   q   r
+// merged: a p b q c r
+// Example 2:
+
+// Input: word1 = "ab", word2 = "pqrs"
+// Output: "apbqrs"
+// Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+// word1:  a   b 
+// word2:    p   q   r   s
+// merged: a p b q   r   s
+// Example 3:
+
+// Input: word1 = "abcd", word2 = "pq"
+// Output: "apbqcd"
+// Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+// word1:  a   b   c   d
+// word2:    p   q 
+// merged: a p b q c   d
+ 
+
+// Constraints:
+
+// 1 <= word1.length, word2.length <= 100
+// word1 and word2 consist of lowercase English letters.
+
+
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+
+// Solutions:
+
+// #1
+const mergeAlternately = function(word1, word2) {
+  let res = ''
+  const maxLen = Math.max(word1.length, word2.length)
+
+
+  for (let i = 0; i < maxLen; i++) {
+    res += word1[i] || ''
+    res += word2[i] || ''
+  }
+
+  return res
+}
+
+// #2
+const mergeAlternately = function(word1, word2) {
+  let res = ''
+  let i = 0
+
+  while (i < word1.length && i < word2.length) {
+    res += word1[i]
+    res += word2[i]
+    i++
+  }
+
+  while (i < word1.length) {
+    res += word1[i]
+    i++
+  }
+
+  while (i < word2.length) {
+    res += word2[i]
+    i++
+  }
+
+  return res
+}
