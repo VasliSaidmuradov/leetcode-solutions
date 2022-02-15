3473f8639827dd5604eecf6cefdd7d60f458fe9b
Vasli Saidmuradov <44390703+VasliSaidmuradov@users.noreply.github.com>
2022-02-15 17:23:46 +0600

    feat: 344-reverse-string - one more solution

--- easy/344-reverse-string.js
+++ easy/344-reverse-string.js
@@ -1,3 +1,5 @@
+// 344-reverse-string
+
 // Write a function that reverses a string. The input string is given as an array of characters s.
 
 // You must do this by modifying the input array in-place with O(1) extra memory.
@@ -40,6 +42,21 @@ const reverseString = function(s) {
     let temp = s[i]
     s[i] = s[j]
     s[j] = temp
+
+    i++
+    j--
+  }
+
+  return s
+}
+
+// #3
+const reverseString = function(s) {
+  let i = 0
+  let j = s.length - 1
+
+  while (i < j) {
+    [s[i], s[j]] = [s[j], s[i]]
 
     i++
     j--
