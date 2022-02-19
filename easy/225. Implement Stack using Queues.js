3be2c91e420f80be05480190d7cdcaf1b9dca559
Vasli Saidmuradov <vasli.said@gmail.com>
2022-02-19 20:28:48 +0600

    feat: 225. Implement Stack using Queues

---
+++ easy/225. Implement Stack using Queues.js
@@ -0,0 +1,111 @@
+// 225. Implement Stack using Queues
+
+// Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+// Implement the MyStack class:
+
+// void push(int x) Pushes element x to the top of the stack.
+// int pop() Removes the element on the top of the stack and returns it.
+// int top() Returns the element on the top of the stack.
+// boolean empty() Returns true if the stack is empty, false otherwise.
+// Notes:
+
+// You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
+// Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
+ 
+
+// Example 1:
+
+// Input
+// ["MyStack", "push", "push", "top", "pop", "empty"]
+// [[], [1], [2], [], [], []]
+// Output
+// [null, null, null, 2, 2, false]
+
+// Explanation
+// MyStack myStack = new MyStack();
+// myStack.push(1);
+// myStack.push(2);
+// myStack.top(); // return 2
+// myStack.pop(); // return 2
+// myStack.empty(); // return False
+ 
+
+// Constraints:
+
+// 1 <= x <= 9
+// At most 100 calls will be made to push, pop, top, and empty.
+// All the calls to pop and top are valid.
+ 
+
+// Follow-up: Can you implement the stack using only one queue?
+
+class Node {
+  constructor (value) {
+    this.value = value
+    this.next = null
+  }
+}
+
+class MyQueue {
+  constructor () {
+    this.head = null
+    this.tail = null
+    this.size = 0
+  }
+
+  enqueue (val) {
+    const newNode = new Node(val)
+    if (!this.size) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      this.tail.next = newNode
+      this.tail = newNode
+    }
+    this.size++
+    return newNode
+  }
+
+  dequeue () {
+    if (!this.size) return null
+    const removed = this.head
+    if (this.head === this.tail) {
+      this.tail = null
+    }
+    this.head = this.head.next
+    this.size--
+    return removed.value
+  }
+
+  isEmpty () {
+    return !this.size
+  }
+}
+
+const MyStack = function() {
+  this.stack = new MyQueue()
+}
+
+MyStack.prototype.push = function(x) {
+  this.stack.enqueue(x)
+}
+
+MyStack.prototype.pop = function() {
+  let i = this.stack.size
+
+  while (i > 1) {
+    this.stack.enqueue(this.stack.dequeue())
+    i--
+  }
+
+  return this.stack.dequeue()
+}
+
+MyStack.prototype.top = function() {
+  return this.stack.tail.value
+}
+
+MyStack.prototype.empty = function() {
+  return this.stack.isEmpty()
+}
