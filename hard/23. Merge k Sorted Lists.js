869456ba227712cf94f9d9d09bdcab7feadd2833
Vasli Saidmuradov <vasli.said@gmail.com>
2022-03-14 12:13:33 +0600

    feat: 71. Simplify Path

--- hard/23. Merge k Sorted Lists.js
+++ hard/23. Merge k Sorted Lists.js
@@ -80,15 +80,15 @@ const mergeTwoLists = function(list1, list2) {
   let cur = dummy
 
   while (list1 && list2) {
-  if(list1.val > list2.val) {
-  cur.next = list2
-  list2 = list2.next
-  } else {
-  cur.next = list1;
-  list1 = list1.next
-  }
+    if(list1.val > list2.val) {
+      cur.next = list2
+      list2 = list2.next
+    } else {
+      cur.next = list1;
+      list1 = list1.next
+    }
 
-  cur = cur.next
+    cur = cur.next
   }
 
   cur.next = list1 || list2
