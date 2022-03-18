deb029a35611f6a4999f238f332a57210e68573b
Vasli Saidmuradov <vasli.said@gmail.com>
2022-03-19 00:32:04 +0600

    feat: 496. Next Greater Element I

---
+++ easy/496. Next Greater Element I.js
@@ -0,0 +1,78 @@
+// 496. Next Greater Element I
+
+// The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
+
+// You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+
+// For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. If there is no next greater element, then the answer for this query is -1.
+
+// Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+
+ 
+
+// Example 1:
+
+// Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+// Output: [-1,3,-1]
+// Explanation: The next greater element for each value of nums1 is as follows:
+// - 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+// - 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
+// - 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+// Example 2:
+
+// Input: nums1 = [2,4], nums2 = [1,2,3,4]
+// Output: [3,-1]
+// Explanation: The next greater element for each value of nums1 is as follows:
+// - 2 is underlined in nums2 = [1,2,3,4]. The next greater element is 3.
+// - 4 is underlined in nums2 = [1,2,3,4]. There is no next greater element, so the answer is -1.
+ 
+
+// Constraints:
+
+// 1 <= nums1.length <= nums2.length <= 1000
+// 0 <= nums1[i], nums2[i] <= 104
+// All integers in nums1 and nums2 are unique.
+// All the integers of nums1 also appear in nums2.
+ 
+
+// Follow up: Could you find an O(nums1.length + nums2.length) solution?
+
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+
+// #1
+const nextGreaterElement1 = function(nums1, nums2) {
+  for (let i = 0; i < nums1.length; i++) {
+    const idx = nums2.indexOf(nums1[i])
+
+    let j = idx + 1
+    nums1[i] = -1
+    while (j < nums2.length) {
+      if (nums2[idx] < nums2[j]) {
+        nums1[i] = nums2[j]
+        break
+      }
+      j++
+    }
+  }
+
+  return nums1
+}
+
+
+// #2
+const nextGreaterElement2 = function(nums1, nums2) {
+  const getNextGreater = (num) => {
+    let index = nums2.indexOf(num)
+    for (let i = index; i < nums2.length; i++) {
+      if (nums2[i] > num) return nums2[i]
+    }
+    return -1
+  }
+
+  return nums1.map(getNextGreater)
+}
