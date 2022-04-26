46aa13f65710507f27c1538ad4d44df61496646e
Vasli Saidmuradov <vasli.said@gmail.com>
2022-04-26 23:38:21 +0600

    feat: 125. Valid Palindrome - one more solution

--- easy/125. Valid Palindrome.js
+++ easy/125. Valid Palindrome.js
@@ -36,8 +36,39 @@
 
 // Solutions:
 
-// #1
+// #0 - Time O(n) Space O(1)
 const isPalindrome = function(s) {
+  let left = 0
+  let right = s.length - 1
+
+  while (left < right) {
+    while (left < right && !isAlNum(s[left])) {
+      left++
+    }
+    while (right > left && !isAlNum(s[right])) {
+      right--
+    }
+    if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+      return false
+    }
+
+    left++
+    right--
+  }
+
+  return true
+}
+  
+  const isAlNum = (char) => {
+    const code = char.charCodeAt(0)
+
+    return (code >= 48 && code <= 57) || (code >= 65 && code <= 90) || (code >= 97 && code <= 122)
+  }
+
+
+
+// #1
+const isPalindrome1 = function(s) {
   const re = /[a-zA-Z0-9]/gi
 
   const arr = s.match(re)
@@ -46,7 +77,7 @@ const isPalindrome = function(s) {
 }
 
 // #2
-const isPalindrome = s => {
+const isPalindrome2 = s => {
 	const newStr = s.replace(/[^a-zA-Z0-9]/g,'').toLowerCase().split("");
 	
 	let j = 0;
@@ -63,7 +94,7 @@ const isPalindrome = s => {
 }
 
 // #3
-const isPalindrome = function(s) {
+const isPalindrome3 = function(s) {
   let str = s.replace(/[^A-Za-z0-9]/g,"").toLowerCase()
   let left = 0
   let right = str.length - 1
