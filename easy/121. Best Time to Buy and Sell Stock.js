b21f2c4cf961235d2e0ecb076ee3ae0d64f29c40
Vasli Saidmuradov <vasli.said@gmail.com>
2022-04-30 11:25:28 +0600

    feat: 121. Best Time to Buy and Sell Stock - oms

--- easy/121. Best Time to Buy and Sell Stock.js
+++ easy/121. Best Time to Buy and Sell Stock.js
@@ -51,7 +51,7 @@ const maxProfit = function(prices) {
 }
 
 // #2
-const maxProfit = function(prices) {
+const maxProfit2 = function(prices) {
   let maxProfit = 0
   let i = 0
   let j = 1
@@ -69,3 +69,30 @@ const maxProfit = function(prices) {
 
   return maxProfit
 }
+
+// #3
+const maxProfit3 = function(prices) {
+  let min = prices[0]
+  let maxP = 0
+  for (let p of prices) {
+    if (p < min) min = p
+    else if ((p - min) > maxP) maxP = p - min
+  }
+  return maxP
+}
+
+
+
+// PYTHON 3
+// class Solution:
+//     def maxProfit(self, prices: List[int]) -> int:
+//         l, r = 0, 1
+//         maxP = 0
+        
+//         while r < len(prices):
+//             if(prices[l] < prices[r]):
+//                 maxP = max(maxP, prices[r] - prices[l])
+//             else:
+//                 l = r
+//             r += 1
+//         return maxP
