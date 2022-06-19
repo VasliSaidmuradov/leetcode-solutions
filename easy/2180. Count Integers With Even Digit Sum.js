88450e63b70c5f2fd5d35c8ffbf42a30c6587edc
Vasli Saidmuradov <vasli.saidmuradov@kaspi.kz>
2022-06-19 23:50:40 +0600

    feat: 2180. Count Integers With Even Digit Sum

---
+++ easy/2180. Count Integers With Even Digit Sum.js
@@ -0,0 +1,48 @@
+// 2180. Count Integers With Even Digit Sum
+
+// Given a positive integer num, return the number of positive integers less than or equal to num whose digit sums are even.
+
+// The digit sum of a positive integer is the sum of all its digits.
+
+ 
+
+// Example 1:
+
+// Input: num = 4
+// Output: 2
+// Explanation:
+// The only integers less than or equal to 4 whose digit sums are even are 2 and 4.    
+// Example 2:
+
+// Input: num = 30
+// Output: 14
+// Explanation:
+// The 14 integers less than or equal to 30 whose digit sums are even are
+// 2, 4, 6, 8, 11, 13, 15, 17, 19, 20, 22, 24, 26, and 28.
+ 
+
+// Constraints:
+
+// 1 <= num <= 1000
+
+// #1
+const countEven = function(num) {
+  let count = 0
+
+  for (let i = num; i > 0; i--) {
+    const str = i + ''
+    let sum = 0
+    let j = 0
+
+    while (j < str.length) {
+      sum += +str[j]
+      j++
+    }
+
+    if (sum % 2 === 0) {
+      count++
+    }
+  }
+
+  return count
+};
