17f4e1de14801c45f5a0a27573b1425498a77a3f
Vasli Saidmuradov <vasli.saidmuradov@kaspi.kz>
2022-06-22 22:59:08 +0600

    feat: 215. Kth Largest Element in an Array

--- medium/215. Kth Largest Element in an Array.js
+++ medium/215. Kth Largest Element in an Array.js
@@ -32,3 +32,82 @@
 const findKthLargest = function(nums, k) {
   return nums.sort((a, b) => b - a)[k - 1]
 }
+
+// #2
+const findKthLargest2 = function(nums, k) {
+  const heap = new MaxHeap()
+
+  for (let num of nums) {
+    heap.add(num)
+  }
+
+  let kthLargest
+  for (let i = 0; i < k; i++) {
+    kthLargest = heap.extractMax()
+  }
+
+  return kthLargest
+};
+
+
+class MaxHeap {
+  constructor() {
+    this.values = [];
+  }
+
+  add(element) {
+    this.values.push(element);
+    let index = this.values.length - 1;
+    const current = this.values[index];
+
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      let parent = this.values[parentIndex];
+
+      if (parent <= current) {
+        this.values[parentIndex] = current;
+        this.values[index] = parent;
+        index = parentIndex;
+      } else break;
+    }
+  }
+
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    this.values[0] = end;
+
+    let index = 0;
+    const length = this.values.length;
+    const current = this.values[0];
+
+    while (true) {
+      let leftChildIndex = 2 * index + 1;
+      let rightChildIndex = 2 * index + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild > current) {
+          swap = leftChildIndex;
+        }
+      }
+
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if ((swap === null && rightChild > current) || (swap !== null && rightChild > leftChild)) {
+          swap = rightChildIndex;
+        }
+      }
+
+      if (swap === null) break;
+
+      this.values[index] = this.values[swap];
+      this.values[swap] = current;
+      index = swap;
+    }
+
+      return max;
+  }
+}
