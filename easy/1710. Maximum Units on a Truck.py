a59a56dcada853672a0c90b8ddbf9ca9f7ea09cf
Vasli Saidmuradov <vasli.saidmuradov@kaspi.kz>
2022-07-01 23:54:25 +0600

    feat: 1710. Maximum Units on a Truck

---
+++ easy/1710. Maximum Units on a Truck.py
@@ -0,0 +1,12 @@
+# 1710. Maximum Units on a Truck
+
+# 1
+class Solution:
+  def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+    boxTypes.sort(key=lambda box: box[1], reverse=True)
+    totalUnits = 0
+    for numberOfBoxes, unitsPerBox in boxTypes:
+      numBoxes = min(truckSize, numberOfBoxes)
+      totalUnits += numBoxes * unitsPerBox
+      truckSize -= numBoxes
+    return totalUnits
