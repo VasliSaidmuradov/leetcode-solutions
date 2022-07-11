ccf29ebd476a6eab680188bf3c930aa019edecf7
Vasli Saidmuradov <vasli.saidmuradov@kaspi.kz>
2022-07-11 11:05:05 +0600

    feat: 70. Climbing Stairs - python

---
+++ easy/70. Climbing Stairs.py
@@ -0,0 +1,15 @@
+# 70. Climbing Stairs
+
+# Video explanation and solution from neetcode: https://www.youtube.com/watch?v=Y0lT9Fck7qI
+
+# 1
+class Solution:
+  def climbStairs(self, n: int) -> int:
+    one, two = 1, 1
+
+    for i in range(n - 1):
+      temp = one
+      one = one + two
+      two = temp
+
+    return one
